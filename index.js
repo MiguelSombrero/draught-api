a3df90a92c3c04129ebd407ab7ec4a723b5831f2
Miika Somero <miika.somero@alfame.com>
2022-01-05 20:04:43 +0200

    added body parser and db

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 require('dotenv').config()
 const Koa = require('koa')
 const Router = require('@koa/router')
+const koaBody = require('koa-body')
 const app = new Koa();
 const { Sequelize, Model, DataTypes } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {},
 })
-
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
-
-main()
 
 class Draught extends Model {}
 Draught.init({
@@ -28,7 +17,7 @@ Draught.init({
     autoIncrement: true
   },
   beverageType: {
-    type: DataTypes.ENUM('beer', 'wine', 'whisky'),
+    type: DataTypes.TEXT,
     allowNull: false
   },
   abv: {
@@ -43,8 +32,10 @@ Draught.init({
   sequelize,
   underscored: true,
   timestamps: false,
-  modelName: 'note'
+  modelName: 'draught'
 })
+
+Draught.sync()
 
 const helloRouter = new Router()
 
@@ -58,13 +49,20 @@ helloRouter
   });
 
 draughtRouter
-  .get('/', (ctx, next) => {
-    ctx.body = 'Hello draught!'
+  .get('/', async (ctx, next) => {
+    ctx.body = await Draught.findAll()
   })
-  .post('/', (ctx, next) => {
-    // ...
+  .post('/', async (ctx, next) => {
+    try {
+      const draught = Draught.build(ctx.request.body)
+      console.log(ctx.request.body)
+      ctx.body = await draught.save()
+    } catch (error) {
+      ctx.throw(400, error)
+    }
   });
 
+app.use(koaBody())
 app.use(helloRouter.routes())
 app.use(draughtRouter.routes())
 
