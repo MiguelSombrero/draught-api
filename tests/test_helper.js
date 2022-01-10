1d2ed29ee6142585feebb4acfbe0cb14684591a4
Miika Somero <miika.somero@alfame.com>
2022-01-10 14:49:06 +0200

    added try catch to test initializations

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -2,21 +2,33 @@ const { Draught, User } = require('../models')
 const bcrypt = require('bcrypt')
 
 const initializeDatabase = async () => {
-  await Draught.destroy({ where: { } })
-  await User.destroy({ where: { } })
+  try {
+    await Draught.destroy({ where: { } })
+    await User.destroy({ where: { } })
+  } catch(error) {
+    console.log('error on initializing test database')
+  }
 }
 
 const initializeUsers = async () => {
-  await User.create({ id: 1, username: 'somero', password: await bcrypt.hash('miika', 10), name: 'miika' })
-  await User.create({ id: 2, username: 'testinen', password: await bcrypt.hash('testi', 10), name: 'testi' })
+  try {
+    await User.create({ id: 1, username: 'somero', password: await bcrypt.hash('miika', 10), name: 'miika' })
+    await User.create({ id: 2, username: 'testinen', password: await bcrypt.hash('testi', 10), name: 'testi' })
+  } catch(error) {
+    console.log('error on initializing test users')
+  }
 }
 
 const initializeDraughts = async () => {
-  await Draught.create({ beverageType: 'whisky', abv: 40.0, volume: 40, userId: 1 })
-  await Draught.create({ beverageType: 'beer', abv: 5.5, volume: 500, userId: 1 })
-  await Draught.create({ beverageType: 'wine', abv: 14.0, volume: 120, userId: 1 })
-  await Draught.create({ beverageType: 'whisky', abv: 43.0, volume: 40, userId: 2 })
-  await Draught.create({ beverageType: 'whisky', abv: 42.0, volume: 40, userId: 2 })
+  try {
+    await Draught.create({ beverageType: 'whisky', abv: 40.0, volume: 40, userId: 1 })
+    await Draught.create({ beverageType: 'beer', abv: 5.5, volume: 500, userId: 1 })
+    await Draught.create({ beverageType: 'wine', abv: 14.0, volume: 120, userId: 1 })
+    await Draught.create({ beverageType: 'whisky', abv: 43.0, volume: 40, userId: 2 })
+    await Draught.create({ beverageType: 'whisky', abv: 42.0, volume: 40, userId: 2 })
+  } catch(error) {
+    console.log('error on initializing test draughts')
+  }
 }
 
 module.exports = {
